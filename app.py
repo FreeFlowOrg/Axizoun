c1b8b1cd1deac0d415d2728d4b8ede8ee49955de
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-09-23 06:03:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -362,11 +362,11 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host='0.0.0.0', port=80)
 
 # Or specify port manually:
 '''
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=80)
 '''
