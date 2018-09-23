6e6579a39dc7f56dbc95adf0d7af69e216ff2465
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-09-23 06:04:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -362,7 +362,7 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
 
 # Or specify port manually:
 '''
